ab6d0142e9ca44b3168bbb09fb9a6a8708b615e2
Konstantin <konstantinm1006@gmail.com>
2023-05-18 12:52:48 +0300

    practical1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-const open = true,
-  close = false;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(open || close);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("один из последних просмотренных фильмов?", ""),
+  b = prompt("на сколько вы его оцениваете?", ""),
+  c = prompt("один из последних просмотренных фильмов?", ""),
+  d = prompt("на сколько вы его оцениваете?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
